b67c634ec5a72f384dd4a7b83ec976872a4bf058
iamsinem <sinem_sb18@hotmail.com>
2024-07-31 21:32:20 +0300

    customerdüzeltme

--- src/app/services/common/models/customer.service.ts
+++ src/app/services/common/models/customer.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@angular/core";
-import { HttpClientService } from "../http-client.service";
-import { Create_Customer } from "../../../contracts/create_customer";
-
+import { Injectable } from '@angular/core';
+import { HttpClientService } from '../http-client.service';
+import { Create_Customer } from '../../../contracts/create_customer';
+import { error } from 'node:console';
+import { HttpErrorResponse } from '@angular/common/http';
+import { firstValueFrom, Observable } from 'rxjs';
 
 
 @Injectable({
@@ -10,15 +12,26 @@ import { Create_Customer } from "../../../contracts/create_customer";
 
 export class CustomerService{
 
+
+
     constructor(private httpClientservice: HttpClientService){
         
     }
 
-    createCustomer(customer:Create_Customer,successCallBack?: ()=>void){
-     this.httpClientservice.post({
-        controller:"customer"
-     },customer).subscribe(result=>{
-        successCallBack();
-     });
+    createCustomer(customer: Create_Customer, successCallBack?: ()=>void, errorCallBack?: (errorMessage:string)=>void){
+        this.httpClientservice.post({
+          controller:"customer"
+        },customer).subscribe(result => {
+          successCallBack();
+        }, (errorResponse:HttpErrorResponse)=>{
+          const _error:Array<{key:string,value:Array<string>}>=errorResponse.error;
+          let message="";
+          /*_error.forEach((v,index)=>{
+         v.value.forEach((_v,_index)=>{
+          message+=`${_v}<br>`;
+          });
+          });
+          errorCallBack(message);*/
+        });
     }
 }
