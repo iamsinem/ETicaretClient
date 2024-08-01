b23c3990a334484c826c2389a1d3f57b3c14b66a
iamsinem <sinem_sb18@hotmail.com>
2024-08-01 17:20:30 +0300

    uiproduct

---
+++ src/app/ui/components/products/ui-sidebar/ui-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-sidebar',
+  templateUrl: './ui-sidebar.component.html',
+  styleUrl: './ui-sidebar.component.scss'
+})
+export class UiSidebarComponent {
+
+}
---
+++ src/app/ui/uicontracts/listProduct.ts
@@ -0,0 +1,10 @@
+export class UiList_Product {
+    productCode: string;
+    productName: string;
+    price: number;
+    manufactureDate: Date;
+    quantity: number;
+    feature1: string;
+    feature2: string;
+    // Diğer ürün özellikleri
+  }
