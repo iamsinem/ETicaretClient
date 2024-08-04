d9ad1512635ca552cc7f7fe06a24c958cd7b34f0
Oktay KAYA <74820176+oktaykaya@users.noreply.github.com>
2024-08-04 22:56:35 +0300

    overflow

--- src/app/admin/components/products/lists/lists.component.ts
+++ src/app/admin/components/products/lists/lists.component.ts
@@ -20,7 +20,7 @@ constructor(private productService:ProductService,
            private alertifyService:AlertifyService,
            private dialogService :DialogService
           ){}
-displayedColumns: string[] = ['id', 'categoryId', 'productCode', 'productName','price','manufactureDate','quantity','feature1','feature2','createdDate','updatedDate','photos','edit','delete'];
+displayedColumns: string[] = ['categoryId', 'productCode', 'productName','price','manufactureDate','quantity','feature1','feature2','createdDate','updatedDate','photos','delete'];
 dataSource : MatTableDataSource<List_Product> = null;
 
 @ViewChild(MatPaginator) paginator: MatPaginator;
